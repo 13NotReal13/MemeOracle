944009bf4a613ef0697e7324ee250ad9fab0d695
Ivan Semikin <i.semikin.94@icloud.com>
2025-07-14 19:04:18 +0200

    Initial Commit

---
+++ MemeOracle/MemeOracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeOracleApp.swift
+//  MemeOracle
+//
+//  Created by Иван Семикин on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemeOracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
